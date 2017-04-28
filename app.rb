d5fda4ac4616f13ec2ec22a9290cc5058a6104ff
Padma Patil <padma@email.com>
2017-04-28 09:22:14 -0700

    add a new index for viewing in Sinatra.  This should allow for users to create new words by inputting them on the index page, and lay the groundwork for future expansion

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require './lib/dictionary'
+require 'pry'
+
+also_reload('lib/**/*.rb')
+
+get('/') do
+  erb(:index)
+end
+
+get('/add-word') do
+  word_input = params.fetch('word_input')
+  Word.new(word_input)
+  @floating_text = "You've corralled a NEW WORD ranch hand: #{word_input}"
+  erb(:index)
+end
